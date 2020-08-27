e939f920b3a2c0ebaed6da6c103d3d0140483525
andy <andy.william.mcdonald@gmail.com>
2020-08-27 11:24:55 +0100

    Updates

---
+++ precache-manifest.df73708af056048dd0490e51b6b285cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5354d4ddee8a0400c2fd7d46ebd839",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "b0e7458a96a6b79ad61f",
+    "url": "/javascript-calculator/static/css/main.356b3318.chunk.css"
+  },
+  {
+    "revision": "9e7b8120a8124d72ad69",
+    "url": "/javascript-calculator/static/js/2.81fc4b00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.81fc4b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e7458a96a6b79ad61f",
+    "url": "/javascript-calculator/static/js/main.28342007.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
